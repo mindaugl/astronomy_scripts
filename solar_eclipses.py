622ddd40e05fb330d66dd43e309352333377d794
mindaugl <mindelek@gmail.com>
2025-04-18 09:18:18 +0800

    Update solar eclipses calculation script by checking if earth is inside antumbra to differentiate Partial and Annular eclipses.

--- solar_eclipses.py
+++ solar_eclipses.py
@@ -1,7 +1,7 @@
 """
 Calculate solar eclipse times - the times when the angle
 between moon and sun centers from earth are the smallest.
-Differentiate between Total/Hybrid and Partial/Annular eclipses.
+Differentiate between Total/Hybrid, Partial and Annular eclipses.
 """
 
 from numpy import arctan2, pi, sin
@@ -42,13 +42,11 @@ ts = load.timescale()
 eph = load("de421.bsp")
 
 start_time = ts.utc(2011, 1, 1)
-end_time = ts.utc(
-    2021,
-    1,
-)
+end_time = ts.utc(2021, 1, 1)
 
 solar_radius_km = 696340.0
 moon_radius_km = 1737.1
+earth_radius_km = ERAD / 1000
 
 sdict = dict(((s.center, s.target), s.spk_segment) for s in eph.segments)
 sun = sdict[0, 10]
@@ -69,7 +67,7 @@ earth_to_sun = s - b - e
 moon_to_earth = e - m
 moon_to_sun = s - b - m
 
-#################
+###########
 
 angle_penumbra = arctan2(moon_radius_km + solar_radius_km, length_of(moon_to_sun))
 origin_pen_from_sun = (
@@ -78,7 +76,7 @@ origin_pen_from_sun = (
 origin_pen = s - origin_pen_from_sun * moon_to_sun / length_of(moon_to_sun)
 pen_to_earth = b + e - origin_pen
 angle_earth_from_pen = angle_between(-moon_to_sun, pen_to_earth)
-angle_earth_radius_pen = arctan2(ERAD / 1000, length_of(pen_to_earth))
+angle_earth_radius_pen = arctan2(earth_radius_km, length_of(pen_to_earth))
 
 angle_diffs_pen = angle_penumbra + angle_earth_radius_pen - angle_earth_from_pen
 is_eclipse_pen = angle_diffs_pen > 0
@@ -98,13 +96,22 @@ distance_from_umbra_cone = sin(angle_earth_from_umbra - angle_umbra) * length_of
     umbra_to_earth
 )
 is_angle_inside_umbra = angle_umbra - angle_earth_from_umbra > 0
-is_earth_radius_inside_umbra = (ERAD / 1000) >= distance_from_umbra_cone
-is_umbra_inside_earth_radius = length_of(umbra_to_earth) <= (ERAD / 1000)
+is_earth_radius_inside_umbra = earth_radius_km >= distance_from_umbra_cone
+is_umbra_inside_earth_radius = length_of(umbra_to_earth) <= earth_radius_km
+
+angle_antumbra = pi - angle_umbra
+distance_from_antumbra_cone = sin(angle_antumbra - angle_earth_from_umbra) * length_of(
+    umbra_to_earth
+)
+is_eclipse_annular = (angle_earth_from_umbra - angle_antumbra > 0) + (
+    distance_from_antumbra_cone <= earth_radius_km
+)
+is_eclipse_annular *= [not x for x in is_umbra_inside_earth_radius]
 
 is_eclipse_total = is_eclipse_pen * (
-    is_umbra_inside_earth_radius
-    + is_angle_inside_umbra
-    + (is_earth_radius_inside_umbra * (angle_earth_from_umbra - angle_umbra < pi / 2))
+    is_angle_inside_umbra
+    + ((is_earth_radius_inside_umbra) * (angle_earth_from_umbra - angle_umbra < pi / 2))
+    + is_umbra_inside_earth_radius
 )
 
 #################
@@ -112,6 +119,8 @@ is_eclipse_total = is_eclipse_pen * (
 for i in range(len(t)):
     if is_eclipse_total[i]:
         print(t[i].tt_strftime(), "Total/Hybrid")
+    elif is_eclipse_annular[i]:
+        print(t[i].tt_strftime(), "Annular")
     elif is_eclipse_pen[i]:
-        print(t[i].tt_strftime(), "Partial/Annular")
+        print(t[i].tt_strftime(), "Partial")
 
